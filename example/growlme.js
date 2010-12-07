011e5c4c823998d1bc850a8c9c75668fc96e6bbe
Philipp Fehre <phil@caffeinatedcoders.com>
2010-12-07 19:09:48 +0100

    some work done, fixed for each loops to work correctly, fixed packet sending

--- example/growlme.js
+++ example/growlme.js
@@ -1,2 +1,14 @@
-var lib = require('../lib/nodrrr');
+// Load Lib
+var nodrrr = require('../lib/nodrrr');
 
+// Create Growling Object
+var Ng = new nodrrr.Nodrrr('localhost', 'growlnotify', ['Command-Line Growl Notification'], ['Command-Line Growl Notification'], null)
+
+// Register as App
+Ng.register();
+
+// Sent a Notification
+Ng.notify('Command-Line Growl Notification', '', 'hi', 0, false);
+
+// Exit
+process.exit();
