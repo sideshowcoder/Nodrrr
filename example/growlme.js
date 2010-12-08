d0179fcc5c21eda047e46bc8769aa84492d62569
Philipp Fehre <phil@caffeinatedcoders.com>
2010-12-08 16:54:19 +0100

    basic packet is correct, hash does not get added correctly -> buf.write() does not write hex see FIXME

--- example/growlme.js
+++ example/growlme.js
@@ -1,14 +1,9 @@
 // Load Lib
 var nodrrr = require('../lib/nodrrr');
-
 // Create Growling Object
 var Ng = new nodrrr.Nodrrr('localhost', 'growlnotify', ['Command-Line Growl Notification'], ['Command-Line Growl Notification'], null)
-
 // Register as App
 Ng.register();
-
 // Sent a Notification
-Ng.notify('Command-Line Growl Notification', '', 'hi', 0, false);
-
-// Exit
+// Ng.notify('Command-Line Growl Notification', '', 'hi', 0, false);
 process.exit();
