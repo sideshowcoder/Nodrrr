5420c375369421cd2141ed657ec1b0f613636041
Philipp Fehre <phil@caffeinatedcoders.com>
2010-12-09 17:21:26 +0100

    Version 0.0.1 missing tests as well as still includes ugly Array.pack method, but for now it works

--- example/growlme.js
+++ example/growlme.js
@@ -1,9 +1,26 @@
 // Load Lib
 var nodrrr = require('../lib/nodrrr');
+
+// Registration Variables
+var host = 'localhost';
+var app = 'growlnotify';
+var allnotifications = ['Command-Line Growl Notification'];
+var defaultnotifications = ['Command-Line Growl Notification'];
+var pass = null;
+
+// Message
+
+var type = 'Command-Line Growl Notification';
+var title = 'Nodrrr';
+var msg =  'Node.js says grrrr';
+var prio = 0;
+var sticky = false;
+
 // Create Growling Object
-var Ng = new nodrrr.Nodrrr('localhost', 'growlnotify', ['Command-Line Growl Notification'], ['Command-Line Growl Notification'], null)
+var Ng = new nodrrr.Nodrrr(host, app, allnotifications, defaultnotifications, pass)
 // Register as App
 Ng.register();
 // Sent a Notification
-// Ng.notify('Command-Line Growl Notification', '', 'hi', 0, false);
+Ng.notify(type, title, msg, prio, sticky);
+// Exit
 process.exit();
